52152b2b3937d3b5880fa07a56e70d6cc3344bbe
anya <anyaAnisa>
2017-12-27 15:48:05 +0100

    add g
    Signed-off-by: anya <anyaAnisa>

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void B (void) {
+    printf("b\n");
+}
+
 int main() {
 	int a, b;
 	scanf ("%d%d", &a, &b);
 	printf("%d\n", a+b);
+    B();
 	return 0;
 }
+
